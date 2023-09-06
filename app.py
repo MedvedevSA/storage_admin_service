3228c45c5222d99d3ab32dcded01bd192df753de
Stanislav Medvedev <medvedevsa97@gmail.com>
2023-09-06 23:44:20 +0300

    init nuxt client

--- app.py
+++ app.py
@@ -1,11 +1,40 @@
+from pathlib import Path
+
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
+from starlette.responses import FileResponse
 
 from config import settings
 from models import init_models
 from utils import include_routes
 
 app = FastAPI()
+
+build_path = Path(__file__).parent / 'client' / 'dist' 
+origins = [
+    "*",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get('/{file}')
+async def catch_dir(req: Request, file: str):
+    if not Path(build_path / file).exists():
+        file = 'index.html'
+    return FileResponse(build_path / file)
+
+
+@app.route('/{full_path:path}')
+async def catch_all(req: Request):
+    return FileResponse(build_path / 'index.html')
 
 
 @app.on_event('startup')
