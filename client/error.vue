c73eeabea43ebc355aae110addd0a99640efbdfd
Stanislav Medvedev <medvedevsa97@gmail.com>
2023-09-07 18:57:20 +0300

    + страница 404

---
+++ client/error.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div class="row justify-center">
+  <div class="column ">
+    <div class="text-h1 q-pa-lg">404</div>
+    <div class="text-h2 q-pa-lg">
+      <NuxtLink to="/" class=""> На главную </NuxtLink>
+    </div>
+  </div>
+  </div>
+</template>
+
+<style scoped></style>
